ebce1bb21abffca66bf95e1f688f507bef048a08
zach <zachshipko@gmail.com>
2026-01-08 22:23:53 -0800

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    opam-nix.url = "github:tweag/opam-nix";
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "opam-nix/nixpkgs";
+  };
+  outputs =
+    {
+      self,
+      flake-utils,
+      opam-nix,
+      nixpkgs,
+    }@inputs:
+    let
+      package = "zenon";
+    in
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        scope = on.buildOpamProject { } package ./. { ocaml-base-compiler = "*"; };
+        overlay = final: prev: {
+          # Your overrides go here
+        };
+      in
+      {
+        legacyPackages = scope.overrideScope overlay;
+
+        packages.default = self.legacyPackages.${system}.${package};
+      }
+    );
+}
